29cd384cf2cf018bf77299a8c848366bb5d9a1b8
Isabel <lusabe2010@gmail.com>
2021-01-15 07:32:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stringlocalization/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.stringlocalization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+
+        ImageView imageView = (ImageView)findViewById(R.id.image_view);
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast, Toast.LENGTH_LONG).show();
+                Log.d(TAG, "onClick: clicked button");
+            }
+        });
+
+
+    }
+}
